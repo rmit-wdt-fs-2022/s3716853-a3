352efba28b691f9dfc44cc3b293dc3a06c91d90d
Matthew Eletva <s3716853@student.rmit.edu.au>
2022-02-07 14:01:23 +1100

    Project Init from visual studio MVC Template

---
+++ Assessment03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assessment03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

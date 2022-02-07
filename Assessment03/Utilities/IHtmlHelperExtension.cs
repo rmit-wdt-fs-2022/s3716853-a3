c1e6f3977ddbac4a94bb9cc604bf7e5f459b789e
Matthew Eletva <s3716853@student.rmit.edu.au>
2022-02-07 20:45:20 +1100

    Removed unused code

--- Assessment03/Utilities/IHtmlHelperExtension.cs
+++ Assessment03/Utilities/IHtmlHelperExtension.cs
@@ -6,14 +6,6 @@ namespace Assessment03.Utilities;
 
 public static class IHtmlHelperExtension
 {
-    public static IHtmlContent CustomTextArea<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
-    {
-        return new HtmlContentBuilder()
-            .AppendHtml(htmlHelper.LabelFor(expression, htmlHelper.DisplayNameFor(expression)))
-            .AppendHtml(htmlHelper.TextAreaFor(expression, new {@class = "form-control"}))
-            .AppendHtml(htmlHelper.ValidationMessageFor(expression, null, new {@class = "text-danger"}));
-    }
-
     public static IHtmlContent CustomDropDown<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression, IEnumerable<SelectListItem> selectList, string? optionLabel = null)
     {
         return new HtmlContentBuilder()
@@ -27,29 +19,6 @@ public static class IHtmlHelperExtension
         return new HtmlContentBuilder()
             .AppendHtml(htmlHelper.LabelFor(expression, htmlHelper.DisplayNameFor(expression)))
             .AppendHtml(htmlHelper.TextBoxFor(expression, new { @class = "form-control" }))
-            .AppendHtml(htmlHelper.ValidationMessageFor(expression, null, new { @class = "text-danger" }));
-    }
-    public static IHtmlContent CustomDateTime<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
-    {
-        return new HtmlContentBuilder()
-            .AppendHtml(htmlHelper.LabelFor(expression, htmlHelper.DisplayNameFor(expression)))
-            .AppendHtml(htmlHelper.TextBoxFor(expression, new { @class = "form-control", type = "datetime-local" }))
-            .AppendHtml(htmlHelper.ValidationMessageFor(expression, null, new { @class = "text-danger" }));
-    }
-
-    public static IHtmlContent CustomDate<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
-    {
-        return new HtmlContentBuilder()
-            .AppendHtml(htmlHelper.LabelFor(expression, htmlHelper.DisplayNameFor(expression)))
-            .AppendHtml(htmlHelper.TextBoxFor(expression, new { @class = "form-control", type = "date" }))
-            .AppendHtml(htmlHelper.ValidationMessageFor(expression, null, new { @class = "text-danger" }));
-    }
-
-    public static IHtmlContent CustomPassword<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
-    {
-        return new HtmlContentBuilder()
-            .AppendHtml(htmlHelper.LabelFor(expression, htmlHelper.DisplayNameFor(expression)))
-            .AppendHtml(htmlHelper.PasswordFor(expression, new { @class = "form-control" }))
             .AppendHtml(htmlHelper.ValidationMessageFor(expression, null, new { @class = "text-danger" }));
     }
 }
